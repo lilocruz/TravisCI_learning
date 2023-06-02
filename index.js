242a17adda4bbb2fa98f671bb2c413ca84d3b266
Michael Sanchez <msanchez@Michael-Sanchez-Lucidworks-Mac-QCVHPLV2QV.local>
2023-06-02 18:09:17 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// Importing the express library
+const express = require('express')
+
+// Initializing an express app
+const app = express()
+
+const port = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+    res.status(200).json('Michael Sanchez Learning Travis!')  
+})
+
+// Exporting the home route so it can be tested
+module.exports = app.listen(port, () => console.log(`The application is running on ${port}`))
